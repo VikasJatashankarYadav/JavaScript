9266491856b267d31f6cc32a8af145e39b037a80
VikasJatashankarYadav <49014164+VikasJatashankarYadav@users.noreply.github.com>
2021-01-17 15:15:54 +0530

    Upload

---
+++ static/script.js
@@ -0,0 +1,19 @@
+// alert("Hi");
+
+function ageInDays(){
+    var birthYear = prompt("Enter the year in which you were born?");
+    if(birthYear){
+    var numberOfDays = (2021 - birthYear)*365;
+    var h2 = document.createElement('h2');
+    var textAnswer = document.createTextNode("You are " + numberOfDays + " days old!");
+    h2.setAttribute('id', 'ageInDays');
+    h2.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h2);
+    document.getElementById('flex-box-result').style.display = 'block';
+    }
+}
+
+function reset(){
+    document.getElementById('ageInDays').remove();
+    document.getElementById('flex-box-result').style.display = 'none';
+}
