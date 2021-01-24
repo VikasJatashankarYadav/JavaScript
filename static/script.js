73aa5c69f7097a85a6f1056a6f28285a7c3bf0ec
Vikas Yadav <vikas071998@gmail.com>
2021-01-24 16:32:11 +0530

    cat

--- static/script.js
+++ static/script.js
@@ -17,3 +17,10 @@ function reset(){
     document.getElementById('ageInDays').remove();
     document.getElementById('flex-box-result').style.display = 'none';
 }
+
+function generateCat(){
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "https://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
+}
